ddef9819866de1f962246c600af5e6a4be59d5cc
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-11-06 11:54:15 +0300

    feat: add extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -436,8 +436,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const start = value.indexOf(' ') + 1;
+  const finish = value.indexOf('!');
+  return value.substring(start, finish);
 }
 
 /**
